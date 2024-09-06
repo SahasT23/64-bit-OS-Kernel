167311f33ef8ffc86a149a3d9969cfb5047a4a2c
SahasT23 <sahas.st01@gmail.com>
2024-09-06 13:13:25 +0100

    Added all of the necessary subroutines and can now enter long mode as well as adding paging. Now works as a 64 bit kernel

--- src/Implementation/x86_64/boot/main.asm
+++ src/Implementation/x86_64/boot/main.asm
@@ -11,6 +11,9 @@ start:
 	call check_cpuid
 	call check_long_mode
 
+	call setup_page_tables
+	call enable_paging
+
 	; print `OK`
 	mov dword [0xb8000], 0x2f4b2f4f
 	hlt
@@ -54,11 +57,58 @@ check_long_mode:
 	jz .no_long_mode
 	
 	ret
-	;
-
 .no_long_mode:
 	mov al, "L"
 	jmp error
+
+; maps the virtual memory address to the physical one
+
+setup_page_tables:
+	mov eax, page_table_l3
+	or eax, 0b11 ; present, writable
+	mov [page_table_l4], eax
+	
+	mov eax, page_table_l2
+	or eax, 0b11 ; present, writable
+	mov [page_table_l3], eax
+
+	mov ecx, 0 ; counter
+.loop:
+
+	mov eax, 0x200000 ; 2MiB
+	mul ecx
+	or eax, 0b10000011 ; present, writable, huge page
+	mov [page_table_l2 + ecx * 8], eax
+
+	inc ecx ; increment counter
+	cmp ecx, 512 ; checks if the whole table is mapped
+	jne .loop ; if not, continue
+
+	ret
+
+
+enable_paging:
+	; pass page table location to cpu
+	mov eax, page_table_l4
+	mov cr3, eax
+
+	; enable PAE
+	mov eax, cr4
+	or eax, 1 << 5
+	mov cr4, eax
+
+	; enable long mode
+	mov ecx, 0xC0000080
+	rdmsr
+	or eax, 1 << 8
+	wrmsr
+
+	; enable paging
+	mov eax, cr0
+	or eax, 1 << 31
+	mov cr0, eax
+
+	ret
 
 error:
 	; prints error message where X is the error code
@@ -69,6 +119,13 @@ error:
 	hlt
 
 section .bss
+align 4096
+page_table_l4:
+	resb 4096
+page_table_l3:
+	resb 4096
+page_table_l2:
+	resb 4096
 stack_bottom:
 	resb 4096 * 4
 stack_top:
